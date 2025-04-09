f2d3f67d05ec0f1846ad1bbbf7027933b5035d97
Youcef <ymoulai24@gmail.com>
2025-04-09 19:28:38 +0100

    I added the static file and took a copy of it outside the website application, changed some things in the index file and password, put a red dot in front of them, added a path and added the os library, linked the html, css and a little bit of js

--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-     return render(request, 'HTML_files/index.html')
+    return render(request, 'HTML_files/index.html')
+
+def forgotPass(request):
+    return render(request, 'HTML_files/forgotPass.html')
 
 # Create your views here.
