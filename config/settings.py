93446de6eba12600f2629977be9c0c4a3772ea86
Abdelghani <abdelghaniimekraz@gmail.com>
2025-04-10 11:48:52 +0100

    changing the STATIC_DIRS to STATICFILES_DIRS

--- config/settings.py
+++ config/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-STATIC_DIRS = [
-  os.path.join(BASE_DIR, 'website/static')
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'website/static'),
 ]
 
 # Default primary key field type
