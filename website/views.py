43c079cd98c81bf9bedf3b315921ed8198e2e30e
Abdelghani <abdelghaniimekraz@gmail.com>
2025-04-10 11:06:51 +0100

    mise a jour

--- website/views.py
+++ website/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 def index(request):
      return render(request, 'HTML_files/index.html')
 
+def Forgot_Pass(request):
+     return render(request, 'HTML_files/forgotPass.html')
 # Create your views here.
