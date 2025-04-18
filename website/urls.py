79f314f7277d73f9bb6e553c6174864201b8e24f
Abdelghani <abdelghaniimekraz@gmail.com>
2025-04-18 18:52:44 +0100

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -2,14 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('ForgotPass', views.ForgotPass, name='ForgotPass'),
-    path('', views.Register, name='Login'),
-=======
     path('', views.homePage, name='homePage'),
-
     path('forgotPass/', views.forgotPass, name='forgotPass'),
     path('login/', views.register, name='login'),
->>>>>>> back_HomePage
 
 ]
