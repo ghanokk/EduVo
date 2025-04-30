ddefbc934573fe6c6dd7438ad236bbff0688c3f7
Youcef <ymoulai24@gmail.com>
2025-04-30 23:39:17 +0100

    riglt data models w zid navbar w footer drthom w7dhom

--- website/views.py
+++ website/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib import messages
+
+
 
 def forgotPass(request):
      return render(request, 'HTML_files/forgotPass.html')
-from django.shortcuts import render
+
+
 
 def register(request):
     return render(request, 'HTML_files/register.html')
 
 def homePage(request):
      return render(request, 'HTML_files/homePage.html')
+
+
 
 
 # Create your views here.
