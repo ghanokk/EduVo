c4bba8d5cbff1b8a6ca63e5dd5cb3af08e4919cc
Abdelghani <abdelghaniimekraz@gmail.com>
2025-05-23 03:53:15 +0100

    configuration

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+app_name = 'jobs'  # bach t3ayet l URLs b namespace
+
+urlpatterns = [
+    path('', views.job_list, name='job_list'),          # page liste ta3 l'offres
+    path('<int:job_id>/', views.job_detail, name='job_detail'),  # page détails ta3 job m3a id
+]
